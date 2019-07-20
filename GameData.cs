83eb0042e28c1e03f7d9deb4b0611d3d597b1ccf
jeff <jeff.lefevre@gmail.com>
2019-07-20 13:21:36 -0600

    Initial project setup

---
+++ GameData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JL_BlackJack
+{
+  class GameData
+  {
+    /* The purpose of this class is to instantiate
+       and track data related to game state. */
+    private enum DeckSuits
+    {
+      Diamonds,
+      Spades,
+      Hearts,
+      Clubs
+    };
+
+    private enum SuitValues
+    {
+      Ace,
+      Jack,
+      Queen,
+      King
+    };
+  }
+}
---
+++ GameLoop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JL_BlackJack
+{
+  class GameLoop
+  {
+    /* The loop's purpose is executing business logic.
+       It will instantiate objects pertaining to
+       game data and change state. */
+    static void Main(string[] args)
+    {
+
+    }
+  }
+}
